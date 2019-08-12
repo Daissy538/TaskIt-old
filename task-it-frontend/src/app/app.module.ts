4936f962d9bf0063ca7a8da5969803d6939eae2a
hilde <hildehamer@hotmail.com>
2019-08-12 13:20:20 +0200

    Styling of registerform

--- task-it-frontend/src/app/app.module.ts
+++ task-it-frontend/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +8,14 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- task-it-frontend/src/app/modules/authentication/authentication.module.ts
+++ task-it-frontend/src/app/modules/authentication/authentication.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { AuthenticationRouting } from './authentication.routing';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CoreModule } from 'src/app/core/core.module';
+
 @NgModule({
-  imports: [CommonModule, AuthenticationRouting],
+  imports: [
+    AuthenticationRouting,
+    MatFormFieldModule,
+    MatInputModule,
+    CoreModule
+  ],
   declarations: [LoginComponent, RegisterComponent]
 })
 export class AuthenticationModule {}
