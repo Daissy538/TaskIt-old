3c973dd5acdf377db572295edd4bb88d5c83ae6f
hilde <hildehamer@hotmail.com>
2019-10-29 09:46:50 +0100

    Base setup Web for creating tasks

---
+++ task-it-frontend/src/app/modules/task/components/task-listItem/task-listItem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-listItem',
+  templateUrl: './task-listItem.component.html',
+  styleUrls: ['./task-listItem.component.css']
+})
+export class TaskListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ task-it-frontend/src/app/modules/task/task.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaksListComponent } from './components/taks-list/taks-list.component';
+import { TaskListItemComponent } from './components/task-listItem/task-listItem.component';
+import { CoreModule } from 'src/app/core/core.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoreModule
+  ],
+  declarations: [TaksListComponent, TaskListItemComponent]
+})
+export class TaskModule { }
